53afb67f5a08e717016ee1b82e7429f1d960483c
Timothy Carstens <intoverflow@gmail.com>
2022-01-01 15:33:25 -0800

    Updated page width

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,12 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    'page_width': '80%',
+}
+
+html_style = 'css/override.css'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
